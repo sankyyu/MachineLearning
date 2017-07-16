031d2bf5340f25308bea6cc3ca882d240a017365
sankyyu <syu10@stevens.edu>
2017-07-16 21:10:15 +0800

    add random forest method

---
+++ randomForest/randomForest.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 16 17:02:34 2017
+
+@author: Sanky
+"""
+
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+
+import pandas as pd
+import numpy as np
+
+iris=load_iris()
+
+df=pd.DataFrame(iris.data,columns=iris.feature_names,dtype=float)
+df['is_train']=np.random.uniform(0,1,len(df))<=0.75
+df['species']=pd.Categorical.from_codes(iris.target,iris.target_names)
+df.head()
+
+train,test=df[df['is_train']==True],df[df['is_train']==False]
+
+features=df.columns[:4]
+clf=RandomForestClassifier(n_jobs=2)
+y=pd.Categorical.from_array(train['species'])
+clf.fit(train[features],y)
+
+preds=clf.predict(test[features])
+pd.crosstab(test['species'],preds,rownames=['actual'],colnames=['preds'])
+
